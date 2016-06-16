c2dfd4f6b1e5ab55a63cc16a76417becdfbf4ca5
jiaowenzheng <jiaowenzheng@jiemian.com>
2016-06-16 14:06:21 +0800

    增加图片及sample实例

--- app/src/main/java/com/jiao/button/MainActivity.java
+++ app/src/main/java/com/jiao/button/MainActivity.java
@@ -3,11 +3,20 @@ package com.jiao.button;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.button.library.CustomButton;
+
 public class MainActivity extends AppCompatActivity {
+
+    private CustomButton mSelectedBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSelectedBtn = (CustomButton) findViewById(R.id.selected_btn);
+        mSelectedBtn.setSelected(true);
+
     }
 }
--- library/src/main/java/com/button/library/CustomButton.java
+++ library/src/main/java/com/button/library/CustomButton.java
@@ -134,6 +134,13 @@ public class CustomButton extends Button {
 
     /**
      * 设置Button背景
+     *
+     * @param normalSolid         正常状态背景填充
+     * @param pressedSolid        按下状态背景填充
+     * @param normalStroke        正常状态边框颜色填充
+     * @param pressedStroke       按下状态边框颜色填充
+     * @param roundButtonRadius   圆角弧度
+     * @param isEnableSelected    是否打开选中状态
      */
     public void setBackGround(int normalSolid, int pressedSolid, int normalStroke, int pressedStroke, int roundButtonRadius, boolean isEnableSelected) {
         normalSolid = getResources().getColor(normalSolid);
@@ -177,6 +184,12 @@ public class CustomButton extends Button {
 
     /**
      * 设置Button背景
+     *
+     * @param normalSolid         正常状态背景填充
+     * @param pressedSolid        按下状态背景填充
+     * @param stroke              边框
+     * @param roundButtonRadius   圆角弧度
+     * @param isEnableSelected    是否打开选中状态
      */
     public void setBackGround(int normalSolid, int pressedSolid, int stroke, int roundButtonRadius, boolean isEnableSelected) {
         normalSolid = getResources().getColor(normalSolid);
@@ -218,6 +231,11 @@ public class CustomButton extends Button {
 
     /**
      * 设置Button背景
+     *
+     * @param normalSolid         正常状态背景填充
+     * @param pressedSolid        按下状态背景填充
+     * @param roundButtonRadius   圆角弧度
+     * @param isEnableSelected    是否打开选中状态
      */
     public void setBackGround(int normalSolid, int pressedSolid, int roundButtonRadius, boolean isEnableSelected) {
         normalSolid = getResources().getColor(normalSolid);
@@ -253,6 +271,9 @@ public class CustomButton extends Button {
 
     /**
      * 设置Button文字颜色
+     *
+     * @param normalTextColor       正常状态颜色
+     * @param selectedTextColor     选中状态颜色
      */
     public void setTextColor(int normalTextColor, int selectedTextColor) {
         normalTextColor = getResources().getColor(normalTextColor);
@@ -286,6 +307,12 @@ public class CustomButton extends Button {
         super.setEnabled(enabled);
     }
 
+    /**
+     * 设置button状态不可用
+     *
+     * @param enabled  true 可用 false 不可用
+     * @param color    不可用状态下的颜色
+     */
     public void setEnabled(boolean enabled,int color) {
         if (enabled) {
             setBackgroundDrawable(selector);
